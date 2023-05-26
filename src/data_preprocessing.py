e300fc97de5d6bded3ae5dabe31652c20910c78c
LWaetzig <lucas@waetzig.net>
2023-05-26 09:24:07 +0200

    create validation set

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,15 +1,45 @@
+import math
 import os
+import random
+import shutil
 
-img_width = 30
-img_height = 30
-n_channels = 3
-batch_size = 32
-n_epochs = 200
-val_batch_size = 32
-class_names = list(range(43))
+import pandas as pd
 
-train_path = os.path.join('data', 'Train')
-test_path = os.path.join('data', 'Test')
 
-all_image_paths_train = list(train_path.glob('*/*'))
-all_image_paths_train = [str(path) for path in all_image_paths_train]
+def create_validation_set():
+    train = pd.read_csv("data/train.csv", index_col=0)
+    validation = pd.DataFrame(columns=train.columns)
+
+    validation_dir = os.path.join("data", "Validation")
+    train_dir = os.path.join("data", "Train")
+
+    if not os.path.exists(validation_dir):
+        os.mkdir(validation_dir)
+
+    n_classes = len(os.listdir(os.path.join("data", "Train")))
+
+    val_classes = random.sample(range(n_classes), math.ceil(n_classes / 4))
+
+    for val_class in val_classes:
+        path = os.path.join(validation_dir, str(val_class))
+        os.makedirs(path) if not os.path.exists(path) else None
+
+        src = os.path.join(train_dir, str(val_class))
+        for file in os.listdir(src):
+            shutil.move(os.path.join(src, file), os.path.join(path, file))
+        
+
+    for i , row in train.iterro():
+        if not os.path.exists(row["Path"]):
+            validation.loc[i, "Width"] = row["Width"]
+            validation.loc[i, "Height"] = row["Height"]
+            validation.loc[i, "Roi.X1"] = row["Roi.X1"]
+            validation.loc[i, "Roi.Y1"] = row["Roi.Y1"]
+            validation.loc[i, "Roi.X2"] = row["Roi.X2"]
+            validation.loc[i, "Roi.Y2"] = row["Roi.Y2"]
+            validation.loc[i, "ClassId"] = row["ClassId"]
+            validation.loc[i, "Path"] = row["Path"].replace("Train", "Validation")
+            train.drop(i, inplace=True)
+
+    train.to_csv("data/train.csv")
+    validation.to_csv("data/validation.csv")
