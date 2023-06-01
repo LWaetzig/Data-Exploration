62ac5dd8e1930d34a5e016faaaf6f62e40016e85
Nicho Link <nicholaselink@gmail.com>
2023-06-02 01:33:29 +0200

    Bugfix and new Comment in create_model() Function

--- model-development/create_model_v7.py
+++ model-development/create_model_v7.py
@@ -68,7 +68,7 @@ num_classes = len(np.unique(train_labels))
 # Funktion zur Modellerstellung (Die verschiedenen Layer festlegen und anschließend Modell testen um während des Trainings zu überwachen)
 def create_model():
     model = keras.Sequential([
-        keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_size[0], image_size[1], 3)),
+        keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_size[0], image_size[1], 1)), # Bei input_shape die letzte Zahl von 1 auf 3 ändern, sollten die Bilder NICHT in Graustufen eingelesen werden
         keras.layers.MaxPooling2D((2, 2)),
         keras.layers.Flatten(),
         keras.layers.Dense(64, activation='relu'),
