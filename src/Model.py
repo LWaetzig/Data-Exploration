0e07fc55e1949d593c425d5dc53dfe87b013eb5c
Nicho Link <nicholaselink@gmail.com>
2023-06-02 14:49:11 +0200

    some additions

--- src/Model.py
+++ src/Model.py
@@ -3,17 +3,17 @@ from keras import load_model
 class Model():
 
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self):
         pass
 
     def create_model():
         pass
 
 
-    def evaluate_model():
+    def evaluate_model(self, path):
+        img = load_im
         pass
 
 
-    def load_model(self):
-        model = load_model(self.path)
+    def load_model(self, path):
+        model = load_model(path)
