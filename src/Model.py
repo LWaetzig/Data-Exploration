8d87509853b5d6edd61feea3ae9973ef5481f6bd
LWaetzig <lucas@waetzig.net>
2023-06-02 10:41:45 +0200

    create own file for model

---
+++ src/Model.py
@@ -0,0 +1,19 @@
+from keras import load_model
+
+class Model():
+
+
+    def __init__(self, path):
+        self.path = path
+        pass
+
+    def create_model():
+        pass
+
+
+    def evaluate_model():
+        pass
+
+
+    def load_model(self):
+        model = load_model(self.path)
